d1a0f9fd7042a35c6ff93fbedfef51da64468057
altbrace <dim203286@gmail.com>
2022-08-15 03:26:50 +0700

    format fix

--- run.sh
+++ run.sh
@@ -67,11 +67,7 @@ conn ikev2-vpn
     ike=chacha20poly1305-sha512-curve25519-prfsha512,aes256gcm16-sha384-prfsha384-ecp384,aes256-sha1-modp1024,aes128-sha1-modp1024,3des-sha1-modp1024!
     esp=chacha20poly1305-sha512,aes256gcm16-ecp384,aes256-sha256,aes256-sha1,3des-sha1!" > ./ipsec/ipsec.conf
 
-    echo -e ": RSA "server-key.pem"
+echo -e ": RSA "server-key.pem"
 $username : EAP \"$password\"" > ./ipsec/ipsec.secrets
-
-
-
-
 
 
